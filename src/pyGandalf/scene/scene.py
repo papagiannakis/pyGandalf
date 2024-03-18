b3d4bd79aafc18d5dcc112d6171ff5c653bb7ead
John Petropoulos <johnaipeia@gmail.com>
2024-03-18 20:59:26 +0200

    Minor docs update.

--- src/pyGandalf/scene/scene.py
+++ src/pyGandalf/scene/scene.py
@@ -3,6 +3,8 @@ from pyGandalf.systems.system import System
 from pyGandalf.utilities.logger import logger
 
 class Scene():
+    """A class that represents a scene which contains all the entities, their components and the registered systems.
+    """
     def __init__(self, name = 'UnnamedScene'):
         self.entity_components = {}
         self.component_arrays = {}
@@ -10,15 +12,34 @@ class Scene():
         self.systems = []
         self.name = name
     
-    def enroll_entity(self):
+    def enroll_entity(self) -> Entity:
+        """Creates and enrolls a new entity to the scene.
+
+        Returns:
+            Entity: The newly created entity.
+        """
         entity = Entity()
         self.entities.append(entity)
         return entity
 
-    def get_entities(self):
+    def get_entities(self) -> list[Entity]:
+        """Returns a list containing all the enrolled entities of the scene.
+
+        Returns:
+            list[Entity]: A list containing all the enrolled entities of the scene.
+        """
         return self.entities
     
     def add_component(self, entity: Entity, component):
+        """Adds to the specified entity the given component, if the entity does not already have a component of that type. Also adds the component and entity to the systems that operate on the compoent type.
+
+        Args:
+            entity (Entity): The entity to add the given component to.
+            component (type(component)): The component to add to the given entity.
+
+        Returns:
+            type(component): The component that was added or the already existing one if the entity already has a component of that type.
+        """
         # Create new component reference array for entity if it does not exist
         if entity.id not in self.entity_components:
             self.entity_components[entity.id] = {}
@@ -53,6 +74,12 @@ class Scene():
         return component
     
     def remove_component(self, entity: Entity, component_type: type):
+        """Removes from the specified entity the component of the given type (if the entity has a component of that type) and from all the systems that operate on the component type.
+
+        Args:
+            entity (Entity): The entity to remove the component from.
+            component_type (type): The type of the component to remove from the entity.
+        """
         if self.has_component(entity, component_type) is False:
             return
 
@@ -65,9 +92,27 @@ class Scene():
         self.entity_components[entity.id].pop(component_type)
 
     def has_component(self, entity: Entity, component_type: type) -> bool:
+        """Returns whether or not the specified entity has a compoent of the given type.
+
+        Args:
+            entity (Entity): The entity to check.
+            component_type (type): The type of component to check.
+
+        Returns:
+            bool: ```True``` if the entity has the component or ```False``` otherwise.
+        """
         return component_type in self.entity_components.get(entity.id)
     
     def get_component(self, entity: Entity, component_type: type):
+        """Returns a component of the specified type from the given entity or ```None``` if the entity does not have a component of the specified type.
+
+        Args:
+            entity (Entity): The entity to get the component from.
+            component_type (type): The type of component to get from the given entity.
+
+        Returns:
+            component: The component of the specified type from the given entity or ```None``` if the entity does not have a component of the specified type.
+        """
         if component_type in self.entity_components[entity.id]:
             component_index = self.entity_components[entity.id][component_type]
             return self.component_arrays[component_type][component_index]
@@ -75,12 +120,30 @@ class Scene():
             return None
     
     def get_entity_component_references(self, entity: Entity):
+        """Returns a dictionary with keys the component type and values the indices to the component arrays for the specified entity.
+
+        Args:
+            entity (Entity): The entity to get the component references for.
+
+        Returns:
+            dict[type, list[int]]: A dictionary with keys the component type and values the indices to the component arrays for the specified entity.
+        """
         return self.entity_components.get(entity.id, {})
     
     def get_components_array(self):
+        """Returns a dictionary with keys the compoennt type and values the component arrays.
+
+        Returns:
+            dict[type, list[component]]: A dictionary with keys the component type and values the component arrays.
+        """
         return self.component_arrays
     
-    def register_system(self, system):
+    def register_system(self, system: System):
+        """Registers a system to the scene. If called at runtime it will also start the system.
+
+        Args:
+            system (System): The system to register to the scene.
+        """
         self.systems.append(system)
 
         system.filter(self)
@@ -91,15 +154,30 @@ class Scene():
             system.on_create_base()
 
     def get_systems(self) -> list[System]:
+        """Return a list containing all the systems of the scene.
+
+        Returns:
+            list[System]: A list containing all the systems of the scene.
+        """
         return self.systems
     
     def get_system(self, system_type: type) -> (System | None):
+        """Returns a system from the scene of the specified type.
+
+        Args:
+            system_type (type): The type of the system to get.
+
+        Returns:
+            System: A system from the scene of the specified type or ```None``` if the scene does not have a system with the specified type.
+        """
         for system in self.systems:
             if type(system) is system_type:
                 return system
         return None
     
     def on_create(self):
+        """Called on the first frame and calls all the on_create_base method from all the registered systems of the scene.
+        """
         from pyGandalf.core.application import Application
 
         Application().set_is_running(True)
@@ -107,6 +185,11 @@ class Scene():
         for system in self.systems:
             system.on_create_base()
 
-    def on_update(self, ts):
+    def on_update(self, ts: float):
+        """Called every frame and calls all the on_update_base method from all the registered systems of the scene.
+
+        Args:
+            ts (float): The application timestep.
+        """
         for system in self.systems:
             system.on_update_base(ts)
