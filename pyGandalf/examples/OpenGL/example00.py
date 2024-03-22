fa196ee55bcb9ac983ddadb7f03625309cd8681a
John Petropoulos <johnaipeia@gmail.com>
2024-03-22 19:21:40 +0200

    Minor update to example00.

--- pyGandalf/examples/OpenGL/example00.py
+++ pyGandalf/examples/OpenGL/example00.py
@@ -22,7 +22,6 @@ from pyGandalf.utilities.opengl_shader_lib import OpenGLShaderLib
 
 from pyGandalf.utilities.logger import logger
 
-import OpenGL.GL as gl
 import glfw
 import numpy as np
 import glm
@@ -53,15 +52,7 @@ class MovementSystem(System):
         def on_close_callback():
             print(f'Entity with name: {info.tag} says goodbye')
 
-        def on_pointer_entered(entered):
-            if info.tag == 'e1':
-                if entered == 1:
-                    print(f'The pointer got inside')
-                else:
-                    print(f'The pointer got outside')
-
         EventManager().attach_callback(EventType.WINDOW_CLOSE, on_close_callback)
-        EventManager().attach_callback(EventType.CURSOR_ENTER, on_pointer_entered, True)
 
     def on_update(self, ts, entity: Entity, components):
         """
@@ -70,13 +61,13 @@ class MovementSystem(System):
         movement, transform, info = components
 
         if InputManager().get_key_down(glfw.KEY_1):
-            if info.tag == 'e1': movement.selected = True
+            if info.tag == 'e3': movement.selected = True
             else: movement.selected = False
         if InputManager().get_key_down(glfw.KEY_2):
-            if info.tag == 'e2': movement.selected = True
+            if info.tag == 'e1': movement.selected = True
             else: movement.selected = False
         if InputManager().get_key_down(glfw.KEY_3):
-            if info.tag == 'e3':movement.selected = True
+            if info.tag == 'e2':movement.selected = True
             else: movement.selected = False
 
         if movement.selected:
@@ -157,7 +148,7 @@ def main():
 
     # Register components to entity2
     scene.add_component(entity2, InfoComponent("e2"))
-    scene.add_component(entity2, TransformComponent(glm.vec3(2, 0, 0), glm.vec3(0, 0, 0), glm.vec3(1, 1, 1)))
+    scene.add_component(entity2, TransformComponent(glm.vec3(0.5, -1.25, 0), glm.vec3(0, 0, 0), glm.vec3(1, 1, 1)))
     scene.add_component(entity2, LinkComponent(entity1))
     scene.add_component(entity2, StaticMeshComponent('', [vertices], None))
     scene.add_component(entity2, MaterialComponent('M_Yellow_Simple'))
@@ -166,7 +157,7 @@ def main():
     # Register components to entity3
     scene.add_component(entity3, InfoComponent("e3"))
     scene.add_component(entity3, TransformComponent(glm.vec3(-2, 0, 0), glm.vec3(0, 0, 0), glm.vec3(1, 1, 1)))
-    scene.add_component(entity3, LinkComponent(entity1))
+    scene.add_component(entity3, LinkComponent(root))
     scene.add_component(entity3, StaticMeshComponent('', [vertices, texture_coords], None))
     scene.add_component(entity3, MaterialComponent('M_Blue_Textured'))
     scene.add_component(entity3, MovementComponent())
@@ -186,6 +177,14 @@ def main():
 
     # Add scene to manager
     SceneManager().add_scene(scene)
+
+    def on_pointer_entered(entered):
+        if entered == 1:
+            print(f'The pointer got inside')
+        else:
+            print(f'The pointer got outside')
+
+    EventManager().attach_callback(EventType.CURSOR_ENTER, on_pointer_entered, True)
 
     # Start application
     Application().start()
